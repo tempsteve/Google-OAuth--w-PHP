e03587b492cc6c213b693535998034eb9c5aae39
tempsteve <tempsteve1024@gmail.com>
2019-02-02 01:14:27 +0800

    Add files via upload

--- callback.php
+++ callback.php
@@ -1,19 +1,19 @@
 <?php
-if(isset($_GET["id_token"])) {
-	$token = $_GET["id_token"];
-	$json = file_get_contents("https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=".$token);
-	//Get a JSON from response
-	$result = json_decode($json);
-	
-	//Because the default scope is email, so we'll get the email from user's Google account
-	$account = $result->email;
+if (isset($_GET["id_token"])) {
+    $token = $_GET["id_token"];
+    $json = file_get_contents("https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=".$token);
+    //Get a JSON from response
+    $result = json_decode($json);
+
+    //Because the default scope is email, so we'll get the email from user's Google account
+    $account = $result->email;
 }
 ?>
 <script lang="javascript">
-if(window.location.hash) {
-	//Google will response an id_token to the callback page, then use Javascript to redirect again and decode the JSON id_token
-	var hash = window.location.hash;
-	var split = hash.split("=");
-	window.location = "./callback.php?id_token="+split[1];
+if (window.location.hash) {
+    //Google will response an id_token to the callback page, then use Javascript to redirect again and decode the JSON id_token
+    var hash = window.location.hash;
+    var split = hash.split("=");
+    window.location = "./callback.php?id_token=" + split[1];
 }
 </script>
